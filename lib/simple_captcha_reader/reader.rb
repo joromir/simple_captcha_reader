fea9ace782a21f7dbfe079feafc552471066f5c4
Georgi Kostov <internal.sun@yahoo.bg>
2017-09-30 01:25:21 +0300

    Add basic implementation for SCR::Reader class

--- lib/simple_captcha_reader/reader.rb
+++ lib/simple_captcha_reader/reader.rb
@@ -1,4 +1,14 @@
 module SCR
   class Reader
+    attr_reader :image_path, :reader
+
+    def initialize(image_path:, reader: RTesseract)
+      @image_path = image_path
+      @reader     = reader.new(image_path)
+    end
+
+    def to_s
+      reader.to_s
+    end
   end
 end
--- spec/reader_spec.rb
+++ spec/reader_spec.rb
@@ -1,4 +1,29 @@
 require 'spec_helper'
 
 RSpec.describe SCR::Reader do
+  subject(:reader) { described_class.new(image_path: image_path) }
+
+  describe '#to_s' do
+    let(:image_path) { './lib/samples/testocr.png' }
+
+    it 'returns the expected text for testimage' do
+      expected_sample = [
+        'This is a lot of 12 point text to test the',
+        'ocr code and see if it works on all types',
+        'of file format.',
+        '',
+        'The quick brown dog jumped over the',
+        'lazy fox. The quick brown dog jumped',
+        'over the lazy fox. The quick brown dog',
+        'jumped over the lazy fox. The quick',
+        'brown dog jumped over the lazy fox.'
+      ]
+
+      expect(reader.to_s).to include(*expected_sample)
+    end
+
+    it 'is an instance of String' do
+      expect(reader.to_s).to be_an_instance_of String
+    end
+  end
 end
